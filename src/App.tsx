aba9f1c1dd2d9c0d49187026caf81668bb89988e
manish <ms5978307@gmail.com>
2023-08-26 12:36:12 +0530

    restart

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import Board from "./components/Board";
 import "./styles/main.scss";
 
 const App: React.FC = () => {
     return (
         <>
-            <main>
-                <Board />
-            </main>
+            <main>Restart</main>
         </>
     );
 };
