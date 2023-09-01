318b1bd66f216d928effad09bbe64119a7556694
manish <ms5978307@gmail.com>
2023-09-02 01:50:49 +0530

    some optimzation

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,13 +1,14 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Node from "./Node";
 import { BoardClass } from "../entites/BoardClass";
+import { NodeInterface } from "../utils/specs";
 
 const Board = () => {
-    const boardObj = new BoardClass();
-    const [board, setBoard] = useState(boardObj.getBoard());
+    const boardObj = new BoardClass(20, 20);
+    const [board, setBoard] = useState<NodeInterface[][]>(boardObj.getBoard());
 
     return (
-        <section className="board">
+        <section className="board" onClick={() => boardObj.search(board)}>
             <button>Search</button>
             {board.map((row, rowIdx) => (
                 <div className="row" key={rowIdx}>
--- src/entites/BoardClass.ts
+++ src/entites/BoardClass.ts
@@ -1,54 +1,68 @@
 import { bfs } from "../algorithm/bfs";
+import { getFunctionExecutionTime } from "../utils/regular";
 import { NodeInterface, PosInterface } from "../utils/specs";
 
 export class BoardClass {
     private width: number;
     private height: number;
-    private nodeStartRow: number = 0;
-    private nodeStartCol: number = 0;
-    private nodeFinishRow: number = 9;
-    private nodeFinishCol: number = 9;
+    private startNodePos: PosInterface;
+    private endNodePos: PosInterface;
+    private board: NodeInterface[][];
 
     public constructor(width?: number, height?: number) {
-        this.width = width ? width : Math.floor(window.innerWidth / 25);
-        this.height = height ? height : Math.floor(window.innerHeight / 25);
+        this.width = width ? width : Math.floor(window.innerWidth / 35);
+        this.height = height ? height : Math.floor(window.innerHeight / 35);
+        this.startNodePos = {
+            row: Math.floor(Math.random() * this.width),
+            col: Math.floor(Math.random() * this.height),
+        };
+        this.endNodePos = {
+            row: Math.floor(Math.random() * this.width),
+            col: Math.floor(Math.random() * this.height),
+        };
+        this.board = this.createBoard();
     }
 
-    private generateNodeData = (row: number, col: number): NodeInterface => {
+    private createNode = (row: number, col: number): NodeInterface => {
         return {
             pos: { row, col },
             attr: {
-                isStart: row === this.nodeStartRow && col === this.nodeStartCol,
+                isStart:
+                    row === this.startNodePos.row &&
+                    col === this.startNodePos.col,
                 isFinish:
-                    row === this.nodeFinishRow && col === this.nodeFinishCol,
+                    row === this.endNodePos.row && col === this.endNodePos.col,
                 isSearched: false,
             },
         };
     };
 
-    public getBoard = (): NodeInterface[][] => {
+    private createBoard = (): NodeInterface[][] => {
         const board: NodeInterface[][] = [];
 
         for (let i = 0; i < this.width; i++) {
             const boardRow: NodeInterface[] = [];
-
             for (let j = 0; j < this.height; j++) {
-                boardRow.push(this.generateNodeData(i, j));
+                boardRow.push(this.createNode(i, j));
             }
-
             board.push(boardRow);
         }
 
         return board;
     };
 
-    public search = (
-        board: NodeInterface[][],
-        action: (pos: PosInterface) => void
-    ): void => {
-        const startNode = board[this.nodeStartRow][this.nodeStartCol];
-        const finishNode = board[this.nodeFinishRow][this.nodeFinishCol];
+    public search = (board: NodeInterface[][]): void => {
+        const startNode = board[this.startNodePos.row][this.startNodePos.col];
+        const finishNode = board[this.endNodePos.row][this.endNodePos.col];
 
-        console.log(bfs(board, startNode, finishNode, action));
+        console.log(
+            getFunctionExecutionTime(() => {
+                console.log(bfs(board, startNode, finishNode));
+            })
+        );
+    };
+
+    public getBoard = (): NodeInterface[][] => {
+        return this.board;
     };
 }
